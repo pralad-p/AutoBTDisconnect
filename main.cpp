c8c6a7af592b1b3a337a7af801e6aa704f7e1341
Pralad Prasad <pralad@gmail.com>
2023-03-29 01:44:10 +0200

    Incorporate BT adapter changes for initial version + argparser

--- main.cpp
+++ main.cpp
@@ -1,27 +1,65 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <thread>
+#include <vector>
 #include "BluetoothRadio.h"
 #include "BluetoothDevice.h"
 #include "AudioCheck.h"
+#include "BluetoothAdapter.h"
 
-int main() {
+int main(int argc, char* argv[]) {
     try {
+        // Parse the command-line arguments
+        std::vector<std::string> args(argv, argv + argc);
+        // Remove the first argument, which is the program name
+        args.erase(args.begin());
+
+        // Look for -n and -s flags and their arguments
+        std::string deviceName;
+        std::string script_location;
+        for (auto it = args.begin(); it != args.end(); ++it) {
+            if (*it == "-n" && (it + 1) != args.end()) {
+                deviceName = *(it + 1);
+                it++;
+            }
+            else if (*it == "-s" && (it + 1) != args.end()) {
+                script_location = *(it + 1);
+                it++;
+            }
+        }
+
+        std::wstring device_name(deviceName.begin(), deviceName.end());
+
         BluetoothRadio radio;
         BluetoothDevice device(radio);
-        std::wstring deviceName = L"WH-1000XM3";
-        if (device.IsConnected(deviceName)) {
-            std::wcout << L"Bluetooth device is connected" << std::endl;
-        } else {
-            std::wcout << L"Bluetooth device is not connected" << std::endl;
-        }
-        if (isAudioPlaying()) {
-            std::cout << "Audio is playing" << std::endl;
-        } else {
-            std::cout << "Audio is not playing" << std::endl;
+        auto startTime = std::chrono::steady_clock::now();
+        std::chrono::time_point<std::chrono::steady_clock> currentTime;
+        std::chrono::minutes noAudioThreshold(10);
+
+        while (true) {
+            if (device.IsConnected(device_name)) {
+                if (isAudioPlaying()) {
+                    startTime = std::chrono::steady_clock::now();
+                    std::cout << "Audio still playing. \n";
+                } else {
+                    currentTime = std::chrono::steady_clock::now();
+                    auto diff = currentTime - startTime;
+                    std::cout << "Current gap: " << std::chrono::duration_cast<std::chrono::seconds>(diff).count()
+                              << "\n";
+                    if (currentTime - startTime >= noAudioThreshold) {
+                        reconnectBluetoothAdapter(script_location);
+                        std::cout << "Bluetooth adapter disabled.\n";
+                        exit(0);
+                    }
+                }
+                std::this_thread::sleep_for(std::chrono::minutes(3));
+            } else {
+                std::this_thread::sleep_for(std::chrono::minutes(10));
+            }
         }
     } catch (const std::runtime_error &e) {
         std::cerr << e.what() << std::endl;
+        exit(-1);
     }
-
-    return 0;
 }
